ea4ccf50a3e11c6afe01a42ffecef76b54da2514
Cheyenne Jarman <cheyennej97@gmail.com>
2021-10-12 10:38:41 -0700

    Update coding_demo.R
    Generated data and produced test plot

--- Code/coding_demo.R
+++ Code/coding_demo.R
@@ -1,3 +1,27 @@
 # select source on save - ensures that all code saved runs line by line
 
 # Analytic Workflows: coding demo
+# 
+# additional info about the  code that could be useful for collaborators or
+# future self ...
+# Generate some data, compute linear regression, and produces a figure
+
+
+# Clear workspace and close all grahics devices ---------------------------
+# section headers created using cmd+shift+r
+# recommend starting each script with these commands
+rm(list = ls())
+graphics.off()
+
+
+# Generate some data ------------------------------------------------------
+
+n <- 100
+m <- 2.2
+b <- 1.1
+
+x <- rnorm(n=100, mean=0, sd=1) # independent variable
+noise <- rnorm(n=100, mean=0, sd=1)
+y <- m * x + b + noise
+
+plot(x, y)
